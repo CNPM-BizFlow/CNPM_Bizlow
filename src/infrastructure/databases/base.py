709c7f426487b8d1ab79d1b27ce1ac2aa70f9750
lythienbao <lythienbao171@gmail.com>
2026-01-31 10:48:36 +0700

    Initial commit: Upload source code BizFlow

--- src/infrastructure/databases/base.py
+++ src/infrastructure/databases/base.py
@@ -7,4 +7,4 @@ Base = declarative_base()
 # OOP : object oriented programming
 
 # ERD --> class relational
-# Lập trinhf hướng đối tượng (logic) mapping class -> table (database)
+# Lap trinh huong doi tuong (logic) mapping class -> table (database)
